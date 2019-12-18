0b89d6bd7d8ddf243e38e52d17652b8e98714c79
monguis <jccnnetwork@gmail.com>
2019-12-18 12:43:29 -0700

    added localStorage functionality

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 
+//retrieve info from localStorage
+//if data doesn't exist create new file
+//if the user clicks on highscores pull data and display to user
+//use the list items to display the top players
+//if the user plays save the data in localstorage
+
+
+
+
 var secondCounter = 0;
 var titleText = document.getElementById("questionTitle")
 var timepar = document.getElementById("timer");
@@ -8,6 +17,22 @@ var optionList = document.getElementById("optionList");
 var questionCount = 0;
 var startQuizbtn = document.getElementById("startQuiz");
 var answerDiv = document.getElementById("answerDiv");
+var ScoreArray = [];
+var contentDiv = document.getElementById("content");
+var auxForm = document.createElement("form");
+var scoreLink = document.getElementById("scoresLink");
+var indScore = 0;
+
+
+if (!localStorage.getItem("scoreBoard")) {
+    localStorage.setItem("scoreBoard", JSON.stringify(ScoreArray));
+} else {
+    ScoreArray = JSON.parse(localStorage.getItem("scoreBoard"));
+    console.log(JSON.parse(localStorage.getItem("scoreBoard")));
+
+}
+
+
 
 var questions = [
     {
@@ -19,44 +44,42 @@ var questions = [
         title: "The condition in an if / else statement is enclosed within ____.",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
         answer: "parentheses"
-    },{
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    },{
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    },{
+    },
+    {
         title: "The condition in an if / else statement is enclosed within ____.",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
         answer: "parentheses"
     },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    }
 ];
 
-function renderQuestion(index) {
-    optionList.innerHTML = "";
-    questionTitle.textContent = questions[index].title;
-
-    for (var i = 0; i < questions[index].choices.length; i++) {
-        console.log("entro al for");
-        var li = document.createElement("li");
-        li.textContent = questions[index].choices[i];
-        li.setAttribute("class", "list-group-item list-group-item-action");
-        optionList.appendChild(li);
-    }
-
-}
 
 function renderOutro(str) {
 
     optionList.innerHTML = "";
     questionTitle.textContent = str;
+    auxForm.innerHTML = ('<input class="form-control mb-3 form-control-lg" type="text" placeholder="Enter your name please"><button type="submit" id="submitBtn"  class="btn btn-primary mb-2">Confirm</button>');
+    contentDiv.appendChild(auxForm);
+    li.setAttribute("class", "list-group-item list-group-item-action");
+}
 
-
-
+function addScore(namestr, scorenum) {
+    var newScore = { name: namestr, score: scorenum };
+    ScoreArray.push(newScore);
+    localStorage.setItem("scoreBoard", JSON.stringify(ScoreArray));
 
 }
+
+
 
 function timer() {
     var countdownInterval = setInterval(function () {
@@ -81,26 +104,18 @@ function timer() {
     }, 1000);
 }
 
-optionList.addEventListener("click", function (event) {
-    event.preventDefault();
-    if (event.target.matches("li")) {
-        if (event.target.textContent === questions[questionCount].answer) {
-            printAns(true);
-        } else {
-            printAns(false);
-            secondCounter += -20;
-        }
+function renderQuestion(index) {
+    optionList.innerHTML = "";
+    questionTitle.textContent = questions[index].title;
+
+    for (var i = 0; i < questions[index].choices.length; i++) {
+        var li = document.createElement("li");
+        li.textContent = questions[index].choices[i];
+        li.setAttribute("class", "list-group-item list-group-item-action");
+        optionList.appendChild(li);
     }
 
-    questionCount++;
-    if (questionCount < questions.length) {
-        renderQuestion(questionCount)
-    } else {
-        renderOutro("All Done");
-    }
-
-
-});
+}
 
 function printAns(bool) {
     auxClass = "alert alert-danger";
@@ -117,14 +132,55 @@ function printAns(bool) {
     }, 500);
 }
 
+optionList.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (event.target.matches("li")) {
+        if (event.target.textContent === questions[questionCount].answer) {
+            printAns(true);
+            indScore += 20;
+        } else {
+            printAns(false);
+            secondCounter += -20;
+        }
+    }
+
+    questionCount++;
+    if (questionCount < questions.length) {
+        renderQuestion(questionCount)
+    } else {
+        renderOutro("All Done");
+    }
 
 
+});
 
-startQuizbtn.addEventListener("click", function () {
+startQuizbtn.addEventListener("click", function (event) {
     event.preventDefault();
     questionCount = 0;
     renderQuestion(questionCount)
     secondCounter = 75;
     startQuizbtn.setAttribute("style", "display:none;");
     timer();
+});
+
+
+auxForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addScore(auxForm.querySelector("input").value, indScore)
+    auxForm.innerHTML = "";
+    titleText.textContent = "Welcome";
+    startQuizbtn.setAttribute("style", "display:block;margin: 0 auto");
+});
+
+scoreLink.addEventListener("click", function () {
+
+    if(!localStorage.getItem("scoreBoard")){
+        
+        console.log("no hay nada");
+        return;
+    } else {
+        
+    titleText.textContent = "Scoreboard";
+        console.log("ya hay algo");
+    }
 });
